6c68f5894e2c7df4fda82ded0c09c67ef67fe883
Mayel <git@mayel.space>
2021-05-15 20:51:56 +0200

    route

--- lib/web/fallback_routes.ex
+++ lib/web/fallback_routes.ex
@@ -1,8 +1,6 @@
 defmodule Bonfire.WebPhoenix.FallbackRoutes do
   use Bonfire.WebPhoenix, :router
 
-  alias Bonfire.WebPhoenix.Routes.Helpers, as: Routes
-
 
   # include routes from CommonsPub extensions
 
