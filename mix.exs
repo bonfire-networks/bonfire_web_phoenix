0ad3782017a3493bd1c5198746ed4a93dd33962c
Mayel de Borniol <git@mayel.space>
2022-04-22 08:21:25 +1200

    c

--- mix.exs
+++ mix.exs
@@ -14,12 +14,8 @@ defmodule Bonfire.WebPhoenix.MixProject do
   end
 
   # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      # mod: {Bonfire.Me.FallbackApplication, []},
-      extra_applications: [:logger]
-    ]
-  end
+  def application, do: [extra_applications: [:logger]]
+
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
