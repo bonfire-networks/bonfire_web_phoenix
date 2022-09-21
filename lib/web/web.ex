927280e045d6cf941a1058b36cec03dc4cf06c29
Mayel de Borniol <git@mayel.space>
2022-09-21 20:09:18 +1200

    phx

--- lib/web/web.ex
+++ lib/web/web.ex
@@ -71,7 +71,7 @@ defmodule Bonfire.WebPhoenix do
       use Phoenix.HTML
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
-      import Phoenix.LiveView.Helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
